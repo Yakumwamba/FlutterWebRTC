f70ae2f7633133fcb034d4095634bee730cd7c49
Brian Lemba <brianlemba2@gmail>
2020-11-18 10:26:20 +0200

    Video Calling Added

---
+++ android/app/src/main/kotlin/com/example/yakumwamba_tv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yakumwamba_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "d43c931fdf2545c0a3b9fec8ae31daae";
+const Token = "006d43c931fdf2545c0a3b9fec8ae31daaeIADtTe67RhDF4U9Anpo51XhdSFnj98htuGnf17hFOPNjLvpM5XMAAAAAEAAKvMYLdn2TXwEAAQB2fZNf";
